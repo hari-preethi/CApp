2df1d11ea5a446642df6834c9a54ca9adfb8acf4
Hari Preethi <haripreethicaesar@gmail.com>
2023-07-23 10:24:40 +0530

    modified:   capp.html 	modified:   capp.js

--- capp.js
+++ capp.js
@@ -1,1 +1,11 @@
-const place = ["London","Paris","Sydney","Tokyo"];
+const place = ["London","Paris","Sydney","Tokyo","New York","Rome","Washington DC","Los Angeles","Toronto","Vienna","Melbourne","Beijing","Seoul","Bangkok"];
+
+function displaylist()
+{
+    var str='';
+    for (var i=0; i < place.length;i++)
+    {
+        str += '<option value="'+place[i]+'"></option>';
+    }
+    document.getElementById("place").innerHTML = str;
+}
